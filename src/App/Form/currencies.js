5371e13ab40f055460c40a78e1a8f78860ff0ab7
Kaja Kopczynska <kaja.a.kopczynska@gmail.com>
2023-03-15 11:37:48 +0100

    adding interaction

---
+++ src/App/Form/currencies.js
@@ -0,0 +1,17 @@
+export const currencies = [
+    {
+        short: "PLN",
+        name: "Złoty polski",
+        rate: 1,
+    },
+    {
+        short: "EUR",
+        name: "Euro",
+        rate: 4.71,
+    },
+    {
+        short: "USD",
+        name: "Dolar amerykański",
+        rate: 4.33,
+    },
+];
---
+++ src/App/Form/index.js
@@ -0,0 +1,101 @@
+import { useState } from 'react';
+import { currencies } from './currencies';
+import { Result } from './Result';
+import './style.css';
+
+const Form = () => {
+
+    const [amount, setAmount] = useState("");
+    const [inputCurrency, setInputCurrency] = useState("PLN");
+    const [outputCurrency, setOutputCurrency] = useState(currencies[1].short);
+    const [result, setResult] = useState("");
+
+
+    const calculaterResult = (inputCurrency, outputCurrency, amount) => {
+        const inputRate = currencies.find(({ short }) => short === inputCurrency).rate;
+        const outputRate = currencies.find(({ short }) => short === outputCurrency).rate;
+
+        const outputAmount = amount * inputRate / outputRate;
+
+        setResult({
+            sourceAmount: +amount,
+            targetAmount: outputAmount,
+            inputCurrency,
+            outputCurrency,
+        });
+    }
+
+    const onFormSubmit = (event) => {
+        event.preventDefault();
+        calculaterResult(inputCurrency, outputCurrency, amount);
+    };
+
+    return (
+        <form className="form" onSubmit={onFormSubmit}>
+            <h1 className="form__header"> Przelicznik walut </h1>
+            <p>
+                <label>
+                    <span>
+                        Wpisz kwotę jaką chcesz wymienić i wybierz walutę:
+                    </span>
+                    <input
+                        className="form__amount"
+                        onChange={({ target }) => setAmount(target.value)}
+                        type="number"
+                        step="0.01"
+                        placeholder="0,00"
+                        min="0"
+                        required
+                    />
+                    <select
+                        className="form__currency"
+                        value={inputCurrency}
+                        onChange={({ target }) => setInputCurrency(target.value)}
+                    >
+                        {currencies.map((currency => (
+                            <option
+                                key={currency.short}
+                                value={currency.short}
+                            >
+                                {currency.short}
+                            </option>
+                        )))}
+                    </select>
+                </label>
+
+                <label>
+                    Wybierz walutę jaką chcesz uzyskać:
+                    <select
+                        className="form__currency"
+                        value={outputCurrency}
+                        onChange={({ target }) => setOutputCurrency(target.value)}
+                    >
+                        {currencies.map((currency => (
+                            <option
+                                key={currency.short}
+                                value={currency.short}
+                            >
+                                {currency.short}
+                            </option>
+                        )))}
+                    </select>
+                </label>
+            </p>
+
+            <p className="form__paragraph">
+                <button className="form__button"> Przelicz</button>
+            </p>
+            <p className="form__result">
+                Wynik:
+                <Result result={result} />
+            </p>
+            <p className="form__footer">
+                Przelicznik na podstawie kursu walut z dnia 26.01.2023 r.
+            </p>
+
+        </form >
+    );
+
+};
+
+export default Form;
