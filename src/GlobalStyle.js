de00756cef6cbd20d0b46abe43a5c251e80a4dd1
Kaja Kopczynska <kaja.a.kopczynska@gmail.com>
2023-03-26 22:36:51 +0200

    styled components - GlobalStyle

---
+++ src/GlobalStyle.js
@@ -0,0 +1,32 @@
+import { createGlobalStyle } from "styled-components";
+import background from "./background.jpg";
+
+export const GlobalStyle = createGlobalStyle`
+    html {
+        box-sizing: border-box;
+    }
+
+    *, ::after, ::before {
+      box-sizing: inherit;
+    }
+
+    #root {
+         font-family: 'Montserrat', sans-serif;
+         display: flex;
+         align-items: center;
+         justify-content: center;
+         min-height: 70vh;
+         background-image: url("${background}");
+         background-repeat: no-repeat;
+         background-size: 900px;
+         background-position: center;
+         background-color: rgb(210, 224, 248);
+         margin: 0 auto;
+         text-align: center;
+         color: #84878b;
+         line-height: 2;
+         font-size: 16px;
+         font-weight: 400;
+         padding: 15px;
+    }
+`;
